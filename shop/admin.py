41237ab863814f7d9cb4b9d2e94a4d1df5618ca2
GollaHulikuntappa <gollavenu30@gmail.com>
2025-01-24 16:32:30 +0530

    thirdcommit

--- shop/admin.py
+++ shop/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from shop.product import Product
 from shop.category import Category
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display=['name']
+
 # Register your models here.
 admin.site.register(Product)
-admin.site.register(Category)
+admin.site.register(Category,CategoryAdmin)
--- shop/category.py
+++ shop/category.py
@@ -5,3 +5,5 @@ from django.db import models
 class Category(models.Model):
     name=models.CharField(max_length=1000)
     
+    def __str__(self):
+        return self.name
