93b578a6a68cb9fb027aa79d9462dd29dee318d3
GollaHulikuntappa <gollavenu30@gmail.com>
2025-01-24 13:58:59 +0530

    thirdcommit

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from shop.models import Product
+from shop.product import Product
+from shop.category import Category
 
 # Register your models here.
 admin.site.register(Product)
+admin.site.register(Category)
--- shop/models.py
+++ shop/models.py
@@ -2,8 +2,4 @@ from django.db import models
 
 # Create your models here.
 
-class Product(models.Model):
-    name=models.CharField(max_length=1000)
-    image=models.ImageField(upload_to='img')
-    desc=models.TextField()
-    price=models.IntegerField()
+
