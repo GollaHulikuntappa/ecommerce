8c7e8a9f39fcab98141b228c2d55a4785b3b06d3
GollaHulikuntappa <gollavenu30@gmail.com>
2025-01-25 18:00:47 +0530

    fouthcommit

--- shop/admin.py
+++ shop/admin.py
@@ -4,7 +4,10 @@ from shop.category import Category
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display=['name']
+    
+class ProductAdmin(admin.ModelAdmin):
+    list_display=['name','category','image','price']
 
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Product,ProductAdmin)
 admin.site.register(Category,CategoryAdmin)
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from shop.product import Product
+from shop.category import Category
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1> Welcome to Ecommerce website page</h1>')
+    # return HttpResponse('<h1> Welcome to Ecommerce website page</h1>')
+    result = Product.objects.all()
+    result1= Category.objects.all()
+    products= {'allproducts':result,'allcategories':result1}
+    return render(request,'index.html',products)
+
+
+# def displayProduct(request):
+#     result = Product.objects.all()
+#     products= {'allproducts':result}
+#     return render(request,'product/productpage.html',products)
